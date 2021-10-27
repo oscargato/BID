15cf544b3f914d07c0cb047486e4f433f84ca429
Christian Paez <christianpaez1029@gmail.com>
2021-10-27 15:49:07 -0400

    test

--- src/app/pages/form-routing.module.ts
+++ src/app/pages/form-routing.module.ts
@@ -25,7 +25,7 @@ const routes: Routes = [
     component: FormComponent,
     children: [
       {
-        path: 'f-disponibles/:tramiteId',
+        path: 'f-disponibles/:tramiteId ',
         component: FDisponiblesComponent,
       },
      {
