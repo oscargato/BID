38b02811784e12e000017b96d5d1ac3a3195c644
oscargato <oscardongato@gmail.com>
2022-02-23 17:43:15 -0400

    subir

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -3,8 +3,11 @@ export const environment = {
   appVersion: 'v726demo1',
   USERDATA_KEY: 'authf649fc9a5f55',
   isMockEnabled: true,
-  //apiUrl: 'api',
-  apiUrl: 'http://3.23.158.94:8080/BIDBack/rest/bid',
-  apiFILE:'http://3.23.158.94:8080/BIDBack',
-  // apiUrl: 'mysite.com/api'
+
+  //apiUrl:'https://tramites.municipios.gob.pa:8443/BIDBack/rest/bid',
+  //apiFILE:'https://tramites.municipios.gob.pa:8443/BIDBack',
+  
+  apiUrl: 'https://bnpback.smallshi.com:8443//BIDBack/rest/bid',
+  apiFILE:'https://bnpback.smallshi.com:8443//BIDBack',
+
 };
